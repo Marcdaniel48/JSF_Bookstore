24d5cb5f5896472e4767d194a166934bde138100
Marc-Daniel Dialogo <marcdaniel48@gmail.com>
2018-01-29 19:12:25 -0500

    Added 100 book inserts + removed path field

---
+++ g4w18/target/generated-sources/annotations/com/g4w18/BookAuthors_.java
@@ -0,0 +1,17 @@
+package com.g4w18;
+
+import com.g4w18.Authors;
+import com.g4w18.Books;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-29T06:06:28")
+@StaticMetamodel(BookAuthors.class)
+public class BookAuthors_ { 
+
+    public static volatile SingularAttribute<BookAuthors, Authors> authorId;
+    public static volatile SingularAttribute<BookAuthors, Integer> bookAuthorId;
+    public static volatile SingularAttribute<BookAuthors, Books> bookId;
+
+}
---
+++ g4w18/target/generated-sources/annotations/com/g4w18/InvoiceDetails_.java
@@ -0,0 +1,22 @@
+package com.g4w18;
+
+import com.g4w18.Books;
+import com.g4w18.MasterInvoices;
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-29T06:06:28")
+@StaticMetamodel(InvoiceDetails.class)
+public class InvoiceDetails_ { 
+
+    public static volatile SingularAttribute<InvoiceDetails, BigDecimal> gstRate;
+    public static volatile SingularAttribute<InvoiceDetails, BigDecimal> pstRate;
+    public static volatile SingularAttribute<InvoiceDetails, BigDecimal> hstRate;
+    public static volatile SingularAttribute<InvoiceDetails, Integer> detailId;
+    public static volatile SingularAttribute<InvoiceDetails, MasterInvoices> invoiceId;
+    public static volatile SingularAttribute<InvoiceDetails, BigDecimal> bookPrice;
+    public static volatile SingularAttribute<InvoiceDetails, Books> bookId;
+
+}
