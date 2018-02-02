edf1c901fe75f1d7ed6dc4294ad9e2b80c26114f
Jephthia <jephthia4@hotmail.com>
2018-02-02 15:49:40 -0500

    parallax effect: done

--- g4w18/target/generated-sources/annotations/com/g4w18/InvoiceDetails_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/InvoiceDetails_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-01T18:00:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-02T15:14:55")
 @StaticMetamodel(InvoiceDetails.class)
 public class InvoiceDetails_ { 
 
