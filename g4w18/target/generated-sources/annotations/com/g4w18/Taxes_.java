b7203911e7635cb45fd83f20682aa7f699298017
Jephthia <jephthia4@hotmail.com>
2018-02-01 20:52:55 -0500

    switched from html to xhtml and continued styling

--- g4w18/target/generated-sources/annotations/com/g4w18/Taxes_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/Taxes_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-30T14:29:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-01T18:00:56")
 @StaticMetamodel(Taxes.class)
 public class Taxes_ { 
 
