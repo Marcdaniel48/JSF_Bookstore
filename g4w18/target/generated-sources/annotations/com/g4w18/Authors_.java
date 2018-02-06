49e49a7b797131a1461635a8e0b9ca6afc9e1d25
Jephthia <jephthia4@hotmail.com>
2018-02-06 15:48:30 -0500

    the layout of the main page is in progress

--- g4w18/target/generated-sources/annotations/com/g4w18/Authors_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/Authors_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-02T15:14:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T15:43:41")
 @StaticMetamodel(Authors.class)
 public class Authors_ { 
 
