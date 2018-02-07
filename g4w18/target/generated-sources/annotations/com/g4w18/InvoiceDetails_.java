5f6169a96a3f18b5fd451d8a75d2c70450ab202f
Jephthia <jephthia4@hotmail.com>
2018-02-06 19:33:56 -0500

    now working on the browse section

--- g4w18/target/generated-sources/annotations/com/g4w18/InvoiceDetails_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/InvoiceDetails_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T15:43:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T18:40:58")
 @StaticMetamodel(InvoiceDetails.class)
 public class InvoiceDetails_ { 
 
--- g4w18/target/generated-sources/annotations/com/g4w18/MasterInvoices_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/MasterInvoices_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T15:43:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T18:40:58")
 @StaticMetamodel(MasterInvoices.class)
 public class MasterInvoices_ { 
 
