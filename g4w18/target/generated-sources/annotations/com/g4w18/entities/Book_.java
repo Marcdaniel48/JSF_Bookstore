b892489411301612a56d920a51058cf4323900b8
Marc-Daniel Dialogo <marcdaniel48@gmail.com>
2018-02-09 16:34:55 -0500

    Added template bootfaces styling to login

--- g4w18/target/generated-sources/annotations/com/g4w18/entities/Book_.java
+++ g4w18/target/generated-sources/annotations/com/g4w18/entities/Book_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-09T15:46:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-09T16:32:34")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
