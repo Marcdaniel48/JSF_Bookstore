fb0b69dd3bb7c3602ec947062b04099ee5b6e456
Salman Haidar <Salman Haidar@192.168.2.73>
2018-02-09 17:33:53 -0500

    If search results more than 1, go to appropriate ugly search page. See good results. Removed a lot of junk. -To do  -Formatting the page so its not ugly  -When 1 result, should go to the books details page.  -Use switch to find what search option is used and appropriate query.  -Need to make sure user doesn't choose select by options to search.

--- g4w18/src/main/java/com/g4w18/beans/SearchBackingBean.java
+++ g4w18/src/main/java/com/g4w18/beans/SearchBackingBean.java
@@ -8,25 +8,86 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import com.g4w18.beans.DropDownSearchBean;
 import com.g4w18.beans.SearchBean;
+import com.g4w18.entities.Book;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+
 
 /**
  *
  * @author Salman Haidar
  */
-@Named
-@SessionScoped
+@Named("searchBackingBean")
+@RequestScoped
 public class SearchBackingBean implements Serializable {
-
+    
+    private Logger logger = Logger.getLogger(SearchBackingBean.class.getName());
+    //User's search term
+    private String searchTerm;
+    //Dropdown search option that is chosen
+    private String searchOption;
     @Inject
     private BookJpaController bookJpaController;
-
-    private SearchBean searchBean;
-    private DropDownSearchBean dropDownSearchBean;
-     
+    //Drop down search options
+    private static Map<String,Object> searchOptions;
+    static{
+        searchOptions = new LinkedHashMap<String,Object>();
+        
+        searchOptions.put("Book","Book");
+        searchOptions.put("Title","Title");
+        searchOptions.put("Author","Author");
+        searchOptions.put("ISBN","ISBN");
+        searchOptions.put("Publisher","Publisher");
+    }
+    
+    //private SearchBean searchBean;   
+    //private DropDownSearchBean dropDownSearchBean;
     
     public String searchTitle(){
-        bookJpaController.findBooksByTitle(searchBean.getSearchTerm());
-        
-        return "success";
+       
+        logger.log(Level.INFO, "HEEEEEEEERE "+ searchTerm + "WOOOO "+ searchOption );
+       //List<Book> books = bookJpaController.findBooksByTitle(searchBean.getSearchTerm());
+       List<Book> books = bookJpaController.findBooksByTitle(searchTerm);
+       logger.log(Level.INFO, "HEEEEEEEERE DATATATA : "+ books.size() );
+      
+       
+       if(books.size() > 1)
+           return "resultPlus";
+       else
+           return "resultOne";
     }
+    
+    public List<Book> getMultipleBooks()
+    {
+        List<Book> books = bookJpaController.findBooksByTitle(searchTerm);
+        
+        return books;
+    }
+    
+    public void setSearchTerm(String searchTerm){
+        this.searchTerm = searchTerm;
+    }
+
+    public String getSearchTerm(){
+        return searchTerm;
+    }
+    
+    public Map<String,Object> getSearchOptionValue() {
+		return searchOptions;
+    }
+    
+    public String getSearchOption()
+    {
+        return searchOption;
+    }
+    
+    public void setSearchOption(String searchOption)
+    {
+        this.searchOption = searchOption;
+    }
+    
 }
