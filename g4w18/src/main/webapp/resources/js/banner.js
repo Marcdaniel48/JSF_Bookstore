f5cda856a0b11af73e2a5e7a095ef9bbfdfa17a0
Jephthia <jephthia4@hotmail.com>
2018-02-22 17:14:02 -0500

    homepage layout done

--- g4w18/src/main/webapp/resources/js/banner.js
+++ g4w18/src/main/webapp/resources/js/banner.js
@@ -10,73 +10,72 @@ function goToSlide(_index)
 {
     if(_index === slideIndex) return;
     if(inAnimation) return;
-    
+
     var $currentSlide = $slides.eq(slideIndex);
     $dots.eq(slideIndex).toggleClass("active");
 
     var $nextSlide = $slides.eq(_index);
     $dots.eq(_index).toggleClass("active");
-    
+
     slideIndex = _index;
-    
+
     showSlide($currentSlide, $nextSlide);
 }
 
 function prevSlide()
 {
     if(inAnimation) return;
-    
+
     var $currentSlide = $slides.eq(slideIndex);
     $dots.eq(slideIndex).toggleClass("active");
-    
+
     //Update slide index
     if(slideIndex <= 0)
         slideIndex = (totalSlides - 1);
     else
         slideIndex--;
-    
+
     var $nextSlide = $slides.eq(slideIndex);
     $dots.eq(slideIndex).toggleClass("active");
-    
+
     showSlide($currentSlide, $nextSlide);
 }
 
 function nextSlide()
 {
     if(inAnimation) return;
-    
+
     var $currentSlide = $slides.eq(slideIndex);
     $dots.eq(slideIndex).toggleClass("active");
-    
+
     //Update slide index
     if(slideIndex >= (totalSlides-1))
         slideIndex = 0;
     else
         slideIndex++;
-    
+
     var $nextSlide = $slides.eq(slideIndex);
     $dots.eq(slideIndex).toggleClass("active");
-    
+
     showSlide($currentSlide, $nextSlide);
 }
 
 function showSlide($currentSlide, $nextSlide)
 {
     inAnimation = true;
-    
-    $currentSlide.css("transform", "scale(1.5)");
+
+    $currentSlide.css("transform", "scale(2)");
 
     $currentSlide.animate({opacity: "0"}, 500, 'linear', function()
     {
         $currentSlide.hide();
-        $currentSlide.css({transform: "scale(1)", opacity: "1"});
-        $nextSlide.css("position", "relative");
+        $currentSlide.css("style", "");
         inAnimation = false;
     });
 
     $nextSlide.css({
         "opacity": 0,
-        "transform": "scale(1.5)",
+        "transform": "scale(2.8)",
         "position": "absolute",
         "top": 0,
         "left": 0,
@@ -90,6 +89,7 @@ function showSlide($currentSlide, $nextSlide)
     {
         $nextSlide.css("transform", "scale(1)");
         $nextSlide.animate({opacity: "1"}, 500, 'linear');
+        $nextSlide.css("style", "");
     }, 100);
 }
 
@@ -99,20 +99,20 @@ function init()
     totalSlides = $slides.length;
 
     $slides.eq(slideIndex).show();
-    
-    $(".banner .prev").on("click", prevSlide);
-    $(".banner .next").on("click", nextSlide);
-    
+
+    $("#banner .prev").on("click", prevSlide);
+    $("#banner .next").on("click", nextSlide);
+
     var $dotsContainer = $(".dots-container");
-    
+
     for(var i = 0; i < totalSlides; i++)
     {
         var $dot = $("<span class='dot'></span>");
         $dot.appendTo($dotsContainer);
-        
+
         registerDotClick($dot, i);
     }
-    
+
     function registerDotClick($dot, _index)
     {
         $dot.on("click", function()
@@ -120,9 +120,14 @@ function init()
             goToSlide(_index);
         });
     }
-    
+
     $dots = $(".dot");
     $dots.eq(slideIndex).toggleClass("active");
+
+    setInterval(function()
+    {
+        nextSlide();
+    }, 5000);
 }
 
 window.addEventListener("load", init);
--- g4w18/src/main/webapp/resources/js/onsale.js
+++ g4w18/src/main/webapp/resources/js/onsale.js
@@ -9,7 +9,7 @@ var $prev = null;
 var $next = null;
 
 var leftIndex = 0;
-var rightIndex = 3;
+var rightIndex = 2;
 
 var inAnimation = false;
 
@@ -28,8 +28,8 @@ function getPrevItem()
     var book = allItems[leftIndex];
 
     $item.find(".book-thumbnail").attr("src", book.cover);
-    $item.find(".book-title").text(book.title);
-    $item.find(".book-price").html("<s>"+book.salePrice+"</s> "+book.price);
+    //$item.find(".book-title").text(book.title);
+    //$item.find(".book-price").html("<s>"+book.salePrice+"</s> "+book.price);
 
     return $item;
 }
@@ -51,30 +51,30 @@ function prevItem()
 {
     if(inAnimation) return;
     if(leftIndex === 0) return;
-	
+
     inAnimation = true;
     leftIndex--;
     rightIndex--;
-	
+
     if(leftIndex === 0)
         $prev.toggleClass("disabled");
-	
+
     if(rightIndex === (allItems.length - 2))
         $next.toggleClass("disabled");
-	
+
     $currentItems = $("#book-sales .book");
-	
+
     var $first = getPrevItem();
-	
+
     var $last = $currentItems.last();
 
     $last.animate({opacity: 0}, 600, function()
     {
         $last.remove();
     });
-	
+
     $currentItems.slice(0,$currentItems.length-1).animate({"left": $last.outerWidth()+"px"}, 600);
-	
+
     $first.css(
     {
         position: "absolute",
@@ -82,9 +82,9 @@ function prevItem()
         left: 0,
         opacity: 0
     });
-	
+
     $first.prependTo("#book-sales");
-	
+
     $first.animate({opacity: 1}, 600, function()
     {
         $first.attr("style", "");
@@ -97,27 +97,27 @@ function nextItem()
 {
     if(inAnimation) return;
     if(rightIndex === (allItems.length - 1)) return;
-	
+
     inAnimation = true;
     leftIndex++;
     rightIndex++;
-	
+
     if(leftIndex === 1)
         $prev.toggleClass("disabled");
-	
+
     if(rightIndex === (allItems.length - 1))
         $next.toggleClass("disabled");
-	
+
     $currentItems = $("#book-sales .book");
-	
+
     var $first = $currentItems.first();
 
     var $last = getNextItem();
-	
+
     $first.animate({opacity: 0}, 600);
-	
+
     $currentItems.slice(1).animate({"left": -$first.outerWidth()+"px"}, 600);
-	
+
     $last.css(
     {
         position: "absolute",
@@ -125,10 +125,10 @@ function nextItem()
         right: 0,
         opacity: 0
     });
-	
+
     $currentItems.last().after($last);
-	
-    $last.animate({opacity: 1}, 600, function()
+
+    $last.animate({opacity: 1}, 900, function()
     {
         $last.attr("style", "");
         $currentItems.attr("style", "");
@@ -141,12 +141,12 @@ function init()
 {
     $prev = $("#book-sales .prev");
     $next = $("#book-sales .next");
-	
+
     $prev.on("click", prevItem);
     $next.on("click", nextItem);
-	
+
     $bookTemplate = $("#book-sales .book").eq(0).clone();
-	
+
     for(var i = 0; i < 10; i++)
     {
         allItems.push(new Book(i+"New Title", "/g4w18/javax.faces.resource/book_thumbnail.jpg.xhtml?ln=images", "$"+i+""+i, "$"+i+""+i+""+i));
