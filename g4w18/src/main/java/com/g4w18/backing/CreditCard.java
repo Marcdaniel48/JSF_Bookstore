8024c6649b71363133971ec0b7c4b47b99a3438f
Marc-Daniel Dialogo <marcdaniel48@gmail.com>
2018-02-28 21:44:39 -0500

    Checkout WIP - new: xhtml, backing, props...

---
+++ g4w18/src/main/java/com/g4w18/backing/CreditCard.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g4w18.backing;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Marc-Daniel
+ */
+public class CreditCard implements Serializable{
+    
+    private String number;
+    private String name;
+    private int month;
+    private int year;
+    
+    public CreditCard(String number)
+    {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public CreditCard() 
+    {
+        this("");
+    }
+    
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+    
+    public String getNumber()
+    {
+        return number;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return getNumber();
+    }
+    
+}
---
+++ g4w18/src/main/java/com/g4w18/backing/FinalizationBackingBean.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g4w18.backing;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Marc-Daniel
+ */
+@Named("finalizationBacking")
+@RequestScoped
+public class FinalizationBackingBean implements Serializable
+{
+    private CreditCard creditCard;
+    
+    public CreditCard getCreditCard() {
+        if (creditCard == null) {
+            creditCard = new CreditCard();
+        }
+        return creditCard;
+    }
+    
+    public Collection<SelectItem> getMonthOptions() {   
+        return monthOptions;
+    }
+    
+    private static Collection<SelectItem> monthOptions;
+    static{
+        monthOptions = new ArrayList<>();
+        
+        for(int i = 1; i < 13; i++)
+        {
+            monthOptions.add(new SelectItem(i));
+        }
+    }
+}
