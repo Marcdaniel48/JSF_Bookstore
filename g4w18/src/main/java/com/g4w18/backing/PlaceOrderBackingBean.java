6d30ea67e3edd8a6cf259cab6d5b1c44b2f54a6e
Marc-Daniel Dialogo <marcdaniel48@gmail.com>
2018-03-06 08:07:56 -0500

    When user places order, an invoice is now madegit add .

--- g4w18/src/main/java/com/g4w18/backing/PlaceOrderBackingBean.java
+++ g4w18/src/main/java/com/g4w18/backing/PlaceOrderBackingBean.java
@@ -6,10 +6,20 @@
 package com.g4w18.backing;
 
 import com.g4w18.controllers.ClientJpaController;
+import com.g4w18.controllers.InvoiceDetailJpaController;
+import com.g4w18.controllers.MasterInvoiceJpaController;
+import com.g4w18.controllers.ShoppingCart;
 import com.g4w18.controllers.TaxJpaController;
+import com.g4w18.entities.Book;
 import com.g4w18.entities.Client;
+import com.g4w18.entities.InvoiceDetail;
+import com.g4w18.entities.MasterInvoice;
 import com.g4w18.entities.Tax;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -28,6 +38,12 @@ public class PlaceOrderBackingBean implements Serializable
     private ClientJpaController clientJpaController;
     
     @Inject TaxJpaController taxJpaController;
+    
+    @Inject InvoiceDetailJpaController invoiceJpaController;
+    
+    @Inject MasterInvoiceJpaController masterInvoiceJpaController;
+    
+    @Inject ShoppingCart shoppingCart;
     
     public Client getCurrentClient()
     {
@@ -61,6 +77,49 @@ public class PlaceOrderBackingBean implements Serializable
     {
         double roundedPrice = Double.valueOf(price);
         return Math.round(roundedPrice * 100.) / 100.;
+    }
+    
+    public String createInvoice() throws Exception
+    {
+        Client user = getCurrentClient();
+        Tax tax = taxJpaController.findTaxByProvince(user.getProvince());
+        List<Book> books = shoppingCart.getShoppingCartBooks();
+        
+        
+        MasterInvoice master = new MasterInvoice();
+        master.setClientId(user);
+        master.setSaleDate(Date.valueOf(LocalDate.now()));
+        master.setGrossValue(new BigDecimal(shoppingCart.getSubtotal()));
+        
+        double rate = 1;
+        rate += tax.getGstRate().doubleValue() /100;
+        rate += tax.getPstRate().doubleValue() /100;
+        rate += tax.getHstRate().doubleValue() /100;
+        double net = Math.round((shoppingCart.getSubtotal() * rate) * 100.) / 100.;
+        
+        master.setNetValue(new BigDecimal(net));
+        masterInvoiceJpaController.create(master);
+        
+        for(Book book : books)
+        {
+            InvoiceDetail invoice = new InvoiceDetail();
+            
+            invoice.setInvoiceId(master);
+            invoice.setBookId(book);
+            
+            if(book.getSalePrice().doubleValue() > 0)
+                invoice.setBookPrice(book.getSalePrice());
+            else
+                invoice.setBookPrice(book.getListPrice());
+            
+            invoice.setGstRate(tax.getGstRate());
+            invoice.setHstRate(tax.getHstRate());
+            invoice.setPstRate(tax.getPstRate());
+            
+            invoiceJpaController.create(invoice);
+        }
+        
+        return "/index";
     }
     
 }
