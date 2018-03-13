d90bece51e6a76a0b69a18390e1b1aed08036b1a
Salman Haidar <salmanhaidar9576@gmail.com>
2018-03-13 13:01:39 -0400

    Added all of my new changes.

---
+++ g4w18/src/main/java/com/g4w18/backingbeans/ManagerBookBackingBean.java
@@ -0,0 +1,149 @@
+package com.g4w18.backingbeans;
+
+import com.g4w18.controllers.BookJpaController;
+import com.g4w18.customcontrollers.CustomBookController;
+import com.g4w18.entities.Book;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Handle manager ability to add, edit and remove books. 
+ * 
+ * @author Salman Haidar
+ */
+@Named("managerBookHandling")
+@SessionScoped
+public class ManagerBookBackingBean implements Serializable {
+    
+    private Logger logger = Logger.getLogger(SearchBackingBean.class.getName());
+    //Manager's search term
+    private String searchBook;
+    
+    private String message=null;
+    
+    private Book book;
+    
+    private boolean render=true;
+    private boolean renderEdit=false;
+    
+    @Inject
+    private CustomBookController bookJpaController;
+    
+    public Book getBook()
+    {
+        if(book == null)
+        {
+            book = new Book();
+        }
+        return book;
+    }
+    
+    /**
+     * Edit book.
+     * @return Return to the same page.
+     * @throws Exception 
+     */
+    public String editBook() throws Exception {
+        bookJpaController.edit(book);
+        message = "The book was updated.";
+        return "null";
+    }
+    
+    /**
+     * Search for the book the manager wants. It nothing found, stay in the same page
+     * and display the message.
+     * @return Page to go to
+     */
+    public String search()
+    {
+        int result = 0;
+        logger.log(Level.INFO, "inside of SEARCH HEREHERE: "+ result );
+        result = getBooksByTitle().size();
+        
+        if(result == 0)
+        {
+            logger.log(Level.INFO, "inside of SEARCH of 0: "+ result );
+            message = "fail nothing was returned";
+            return null;
+        }
+        else
+        {
+            logger.log(Level.INFO, "INSIDE of SEARCH of more than 0: "+ result );
+            return "resultEdit";
+        }
+    }
+    
+    /**
+     * Get books with the title provided by the user.
+     * 
+     * @return List of books found
+     */
+    public List<Book> getBooksByTitle()
+    {
+        List<Book> books = bookJpaController.findBooksByTitle(searchBook);
+        
+        return books;
+    }
+    
+    public void setSearchBook(String searchBook)
+    {
+        this.searchBook = searchBook;
+    }
+    
+    public String getSearchBook()
+    {
+        return searchBook;
+    }
+    
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+    
+    public String getMessage()
+    {
+        return message;
+    }
+    
+    public void showEditForm()
+    {
+        renderEdit = true;
+        render = false;
+    }
+    
+    public void hideEditForm()
+    {
+        renderEdit = false;
+        render = true;
+    }
+    
+    public boolean getRender()
+    {
+        return render;
+    }
+    
+    public boolean getRenderForm()
+    {
+        return renderEdit;
+    }
+    
+    public void setRender(boolean render)
+    {
+        logger.log(Level.INFO, "setRender: "+ render );
+        this.render = render;
+    }
+    
+    public void setRenderForm(boolean renderEdit)
+    {
+        logger.log(Level.INFO, "setRenderForm: "+ renderEdit );
+        this.renderEdit = renderEdit;
+    }
+    
+}
