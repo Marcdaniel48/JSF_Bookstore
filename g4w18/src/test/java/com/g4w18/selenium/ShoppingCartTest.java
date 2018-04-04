da57dc2056c9b448a7e7b34e9baf166469246022
Marc-Daniel Dialogo <marcdaniel48@gmail.com>
2018-04-03 23:25:03 -0400

    Completed Selenium testing for the shopping cart

--- g4w18/src/test/java/com/g4w18/selenium/ShoppingCartTest.java
+++ g4w18/src/test/java/com/g4w18/selenium/ShoppingCartTest.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * Tests shopping cart functionality, as well the payment information form when going through the
- * checkout process, using Selenium testing.
+ * Tests shopping cart functionality, as well as successfully proceeding to the checkout phase.
+ * 
  * @author Marc-Daniel
  */
 public class ShoppingCartTest 
@@ -35,6 +35,11 @@ public class ShoppingCartTest
         //driver = new FirefoxDriver();
     }
     
+    /**
+     * Tests the process of adding a book to the shopping cart.
+     * 
+     * @throws Exception 
+     */
     @Test
     public void testAddingBookToShoppingCart() throws Exception
     {
@@ -64,6 +69,10 @@ public class ShoppingCartTest
         wait.until(ExpectedConditions.textToBePresentInElement(bookTitleElements.get(0), "Harry Potter and the Philosopher's Stone"));
     }
     
+    /**
+     * Tests the process of adding then removing a book from the shopping cart.
+     * @throws Exception 
+     */
     @Test
     public void testRemovingBookFromShoppingCart() throws Exception
     {
@@ -86,8 +95,60 @@ public class ShoppingCartTest
         // Remove the added book from the shopping cart. Because there may possibly be multiple books in the shopping cart, make sure to click the remove button at index 0.
         driver.findElements(By.className("remove_from_cart_buttons")).get(0).findElement(By.tagName("button")).click();
         
+        Thread.sleep(1500);
         // Because the added book has been removed, there should be no elements with class name "book_title"
         wait.until(ExpectedConditions.numberOfElementsToBe(By.className("book_title"), 0));
+    }
+    
+    /**
+     * Tests the process of logging in, adding a book to the shopping cart, then proceeding to checkout.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testProceedToCheckoutButton() throws Exception
+    {
+        // To proceed to checkout, the user must first login. Selenium testing for the login page is done in LoginTest.java
+        driver.get("http://localhost:8080/g4w18/login.xhtml");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.titleIs("Sign into your account"));  
+        
+        Thread.sleep(1500);
+        WebElement usernameInputElement = driver.findElement(By.id("login_form:username"));
+        usernameInputElement.clear();
+        usernameInputElement.sendKeys("mdaniels");
+        
+        Thread.sleep(1500);
+        WebElement passwordInputElement = driver.findElement(By.id("login_form:password"));
+        passwordInputElement.clear();
+        passwordInputElement.sendKeys("booktopia");
+        
+        Thread.sleep(1500);
+        driver.findElement(By.id("login_form:login_button")).click();
+        
+        /* 
+            Goes to the book details page of the book with ID 1, and adds it into the shopping cart, because the user cannot proceed to checkout
+            with an empty shopping cart.
+        */
+        Thread.sleep(1500);
+        driver.get("http://localhost:8080/g4w18/bookDetail.xhtml?id=1");
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.titleIs("Book Details"));
+        
+        Thread.sleep(1500);
+        driver.findElement(By.id("add_to_cart_form:add_to_cart_button")).click();
+        
+        // Goes to the shopping cart page, then clicks on the "Proceed to checkout" button, in order to navigate to the page in which the user has to enter his payment information.
+        Thread.sleep(1500);
+        driver.get("http://localhost:8080/g4w18/shopping_cart.xhtml");
+        wait.until(ExpectedConditions.titleIs("Shopping Cart"));
+        
+        Thread.sleep(1500);
+        driver.findElement(By.id("shopping_cart_form:checkoutButton")).click();
+        
+        Thread.sleep(1500);
+        // The user should now be in the checkout page responsible for allowing the user to enter his payment information.
+        wait.until(ExpectedConditions.titleIs("Enter payment information"));
     }
     
     /**
